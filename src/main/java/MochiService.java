be6bb7b2ba38ebf493423bba7708a6e9c4943f85
toriningenn <annamihailova72@yandex.com>
2021-06-15 16:25:40 +0200

    Slow

--- src/main/java/MochiService.java
+++ src/main/java/MochiService.java
@@ -1,20 +1,25 @@
-import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import notion.api.v1.NotionClient;
-import notion.api.v1.model.users.User;
 import okhttp3.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MochiService {
-    private static final OkHttpClient client = new OkHttpClient();
-
-    public static SessionCookie login() {
+    private final static OkHttpClient defaultClient = new OkHttpClient.Builder().build();
+    private final static OkHttpClient client = defaultClient.newBuilder()
+            .readTimeout(15, TimeUnit.DAYS)
+            .connectTimeout(15, TimeUnit.DAYS)
+            .readTimeout(15,TimeUnit.DAYS)
+            .connectionPool(new ConnectionPool(0, 1, TimeUnit.SECONDS))
+            .build();
+//.protocols(List.of(Protocol.HTTP_1_1))
+    public static SessionCookie login() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         String email = UserInfo.askForMochiEmail();
         String password = UserInfo.askForMochiPassword();
@@ -28,12 +33,25 @@ public class MochiService {
                 .addHeader("Accept", "application/json")
                 .addHeader("Content-Type", "application/json")
                 .build();
+        ObjectMapper ow = new ObjectMapper();
+
         try (Response response = client.newCall(request).execute()) {
-            System.out.println(response.headers().get("Set-Cookie"));
-           // CookieJar cj = new CookieJar();
-           // SessionCookie sessionCookie =
-            return mapper.readValue(response.body().string(), SessionCookie.class);
-           // sessionCookie.setRemember_token();
+
+            String encryptedUserId = response.headers().toMultimap()
+                    .get("set-cookie").get(0)
+                    .split("=")[1]
+                    .split(";")[0];
+
+
+            String rememberToken = response.headers().toMultimap()
+                    .get("set-cookie").get(1)
+                    .split("=")[1]
+                    .split(";")[0];
+
+            SessionCookie sessionCookie = mapper.readValue(response.body().string(), SessionCookie.class);
+            sessionCookie.setRemember_token(rememberToken);
+            sessionCookie.setId(encryptedUserId);
+            return sessionCookie;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -46,27 +64,42 @@ public class MochiService {
         String deckId = UserInfo.askForDeckID();
         ArrayList<String> cardsContent = new ArrayList<>();
         ObjectMapper ow = new ObjectMapper();
-        ow.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        System.out.println(ow.writeValueAsString(dictionary.entrySet()));
+
         for (Map.Entry<String, String> entry :
                 dictionary.entrySet()) {
             String front = entry.getKey();
             String back = entry.getValue();
-            cardsContent.add(front+"\\---\\"+back);
+            cardsContent.add(front + " --- " + back);
         }
 
-        for (String content : cardsContent) {
-            Card card = new Card(content, deckId);
-            RequestBody body = RequestBody.create(ow.writeValueAsString(card), null);
-            assert sessionCookie != null;
-            Request request = new Request.Builder()
-                    .url("https://api.mochi.cards/v1/cards")
-                    .post(body)
-                    .addHeader("Cookie","user_id="+sessionCookie.getId()+"; remember_token=2i9JXe9EF-1cZ7yEsH6-2A")
-                    .addHeader("Accept", "application/json")
-                    .addHeader("Content-Type", "application/json")
-                    .build();
-            Response response = client.newCall(request).execute();
-            System.out.println(ow.writeValueAsString(response.body()));
+            for (String content : cardsContent) {
+                System.out.println(content);
+                try {
+                    sendACard(content, deckId, sessionCookie.getId(), sessionCookie.getRemember_token());
+                    //Thread.sleep(3000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        System.out.println("Ready!!!");
+        }
+
+
+
+    public static void sendACard(String content, String deckId, String userID, String rememberToken) throws IOException {
+        RequestBody body = RequestBody.create("{ \"card\": { \"content\": \"" + content + "\", \"deck-id\": \"" + deckId + "\" } }", null);
+        Request request = new Request.Builder()
+                .url("https://api.mochi.cards/v1/cards")
+                .addHeader("Cookie", "user_id=" + userID + "; remember_token=" + rememberToken)
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .post(body)
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/SessionCookie.java
+++ src/main/java/SessionCookie.java
@@ -44,4 +44,32 @@ public class SessionCookie {
     public String getCouchdb_db_name() {
         return couchdb_db_name;
     }
+
+    public String getRemember_token() {
+        return remember_token;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setActivated(Boolean activated) {
+        this.activated = activated;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public void setCouchdb_db_name(String couchdb_db_name) {
+        this.couchdb_db_name = couchdb_db_name;
+    }
 }
